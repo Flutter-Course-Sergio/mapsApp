d02fa9c856e1e04bc0bfee1a012b208104e0d25a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-05 21:14:42 -0700

    Inicio proyecto maps_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MapsApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MapsApp extends StatelessWidget {
+  const MapsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
