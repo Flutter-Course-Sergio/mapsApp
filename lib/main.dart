3275975dc375fb3ff85e8b6c25b0cf3f92180659
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-05 22:44:56 -0700

    GPS Bloc - Eventos, Bloc y Estado

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'blocs/blocs.dart';
 import 'screens/screens.dart';
 
 void main() {
-  runApp(const MapsApp());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (context) => GpsBloc(),
+      )
+    ],
+    child: const MapsApp(),
+  ));
 }
 
 class MapsApp extends StatelessWidget {
@@ -12,6 +21,8 @@ class MapsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: GpsAccessScreen());
+        debugShowCheckedModeBanner: false,
+        title: 'MapsApp',
+        home: GpsAccessScreen());
   }
 }
--- lib/screens/gps_access_screen.dart
+++ lib/screens/gps_access_screen.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/blocs.dart';
 
 class GpsAccessScreen extends StatelessWidget {
   const GpsAccessScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: _AccessButton()),
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<GpsBloc, GpsState>(
+          builder: (context, state) {
+            print('state: $state');
+
+            return !state.isGpsEnabled
+                ? const _EnableGpsMessage()
+                : const _AccessButton();
+          },
+        ),
+      ),
     );
   }
 }
