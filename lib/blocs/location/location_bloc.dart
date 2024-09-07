416fc4d8886b31074d435c8847da8bcecd946af6
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-07 10:59:43 -0700

    Cambiar estado del bloc

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'location_event.dart';
 part 'location_state.dart';
@@ -11,17 +12,24 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
   StreamSubscription<Position>? positionStream;
 
   LocationBloc() : super(const LocationState()) {
-    on<LocationEvent>((event, emit) {});
+    on<OnNewUserLocationEvent>((event, emit) {
+      emit(state.copyWith(
+          lastKnowLocation: event.newLocation,
+          locationHistory: [...state.locationHistory, event.newLocation]));
+    });
   }
 
   Future getCurrentPosition() async {
     final position = await Geolocator.getCurrentPosition();
+    add(OnNewUserLocationEvent(LatLng(position.latitude, position.longitude)));
   }
 
   void startFollowingUser() {
     positionStream = Geolocator.getPositionStream().listen(
       (event) {
         final position = event;
+        add(OnNewUserLocationEvent(
+            LatLng(position.latitude, position.longitude)));
       },
     );
   }
--- lib/blocs/location/location_state.dart
+++ lib/blocs/location/location_state.dart
@@ -2,9 +2,23 @@ part of 'location_bloc.dart';
 
 class LocationState extends Equatable {
   final bool followingUser;
+  final LatLng? lastKnowLocation;
+  final List<LatLng> locationHistory;
 
-  const LocationState({this.followingUser = false});
+  const LocationState(
+      {this.followingUser = false, this.lastKnowLocation, locationHistory})
+      : locationHistory = locationHistory ?? const [];
+
+  LocationState copyWith({
+    bool? followingUser,
+    LatLng? lastKnowLocation,
+    List<LatLng>? locationHistory,
+  }) =>
+      LocationState(
+          followingUser: followingUser ?? this.followingUser,
+          lastKnowLocation: lastKnowLocation ?? this.lastKnowLocation,
+          locationHistory: locationHistory ?? this.locationHistory);
 
   @override
-  List<Object> get props => [followingUser];
+  List<Object?> get props => [followingUser, lastKnowLocation, locationHistory];
 }
