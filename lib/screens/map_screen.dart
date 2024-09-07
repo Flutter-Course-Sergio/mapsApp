868c6b19c990dc2ed44b416d7e919b8de3a5f0d8
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-06 20:30:23 -0700

    Seguir la ubicación del usuario

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MapScreen extends StatelessWidget {
+import '../blocs/blocs.dart';
+
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final locationBloc = BlocProvider.of<LocationBloc>(context);
+    locationBloc.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
