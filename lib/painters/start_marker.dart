a4cc83f57d414319af545af8691fd5e96914c96c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-12 22:35:04 -0700

    style: Sombra y rectángulo del custom marker

--- lib/painters/start_marker.dart
+++ lib/painters/start_marker.dart
@@ -4,13 +4,35 @@ class StartMarkerPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final blackPaint = Paint()..color = Colors.black;
+    final whitePaint = Paint()..color = Colors.white;
 
     const double circleBlackRadius = 20;
+    const double circleWhiteRadius = 7;
 
+    // Black Circle
     canvas.drawCircle(
         Offset(circleBlackRadius, size.height - circleBlackRadius),
         circleBlackRadius,
         blackPaint);
+
+    // White Circle
+    canvas.drawCircle(
+        Offset(circleBlackRadius, size.height - circleBlackRadius),
+        circleWhiteRadius,
+        whitePaint);
+
+    // White Box
+    final path = Path();
+    path.moveTo(40, 20);
+    path.lineTo(size.width - 10, 20);
+    path.lineTo(size.width - 10, 100);
+    path.lineTo(40, 100);
+    canvas.drawShadow(path, Colors.black, 10, false);
+    canvas.drawPath(path, whitePaint);
+
+    // Black Box
+    const blackBox = Rect.fromLTWH(40, 20, 70, 80);
+    canvas.drawRect(blackBox, blackPaint);
   }
 
   @override
